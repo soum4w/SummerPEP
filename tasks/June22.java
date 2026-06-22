aa66ff54511b5a3a09e0b91ec960e0b99d2b92d8
Soum <soumy18a@gmail.com>
2026-06-22 14:01:53 +0530

    added june 22 task

---
+++ tasks/June22.java
@@ -0,0 +1,12 @@
+package tasks;
+
+//create an array and traverse
+
+public class June22 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 5, 4 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
