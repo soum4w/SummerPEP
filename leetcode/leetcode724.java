dc5edd319efe060da579b2cc0083e7eb5243bd3b
Soum <soumy18a@gmail.com>
2026-06-22 13:55:38 +0530

    leetcode 724 added

---
+++ leetcode/leetcode724.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int left = 0, right = 0, sum = 0;
+        for(int ele:nums) sum+=ele;
+        for(int i=0; i<n; i++){
+            right = sum - left - nums[i];
+            if(right == left) return i;
+            left += nums[i];
+        }
+        return -1;
+    }
+}
