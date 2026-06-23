462758ab89ef7241d3c03e28b3c6964c9a242f3d
Soum <soumy18a@gmail.com>
2026-06-23 11:05:35 +0530

    added sum avg max

---
+++ tasks/SumAvgMax.java
@@ -0,0 +1,14 @@
+package tasks;
+
+public class SumAvgMax {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int sum = 0;
+        for(int ele: arr) sum+= ele;
+        System.out.println("Sum: " + sum);
+        System.out.println("Avg: " + sum/arr.length);
+        int max = Integer.MIN_VALUE;
+        for(int ele: arr) max = Math.max(ele, max);
+        System.out.println("Max: " + max);
+    }
+}
